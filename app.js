f85990e8ed50a2fe150fc3e4548883b32f8278ac
erdincozden <ozden@live.com>
2018-02-11 18:09:17 +0300

    initial

--- app.js
+++ app.js
@@ -708,6 +708,7 @@ function greetUserText(userId) {
  *
  */
 function callSendAPI(messageData) {
+	console.log("facebook return message:"+messageData);
 	request({
 		uri: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {
